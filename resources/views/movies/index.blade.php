c79f88f776db58fba29d1c37a65bafc3fde62489
MohamedMOSLAH <mohamed.amine.moslah@gmail.com>
2024-09-08 03:05:12 +0200

    add jetstream

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -2,14 +2,13 @@
 
 @section('content')
 <div class="container">
-    <h1>Films tendances</h1>
+    <h1><b>Films tendances</b></h1>
     <div class="row">
         @foreach($movies as $movie)
             <div class="col-md-4">
                 <div class="card mb-4">
                     <img src="https://image.tmdb.org/t/p/w500/{{ $movie['poster_path'] }}" class="card-img-top" alt="{{ $movie['title'] }}">
                     <div class="card-body">
-                       {{ json_encode($movie) }}
                         <h5 class="card-title">{{ $movie['title'] }}</h5>
                         <p class="card-text">{{ $movie['overview'] }}</p>
                         <a href="{{ route('movies.show', $movie['id']) }}" class="btn btn-primary">Voir les détails</a>
--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,19 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MovieController;
 
+
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/movies', [MovieController::class, 'index']);
-Route::get('/movies/{id}', [MovieController::class, 'show'])->name('movies.show');
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
+Route::get('/movies', [MovieController::class, 'index'])->name('movies.index')->withoutMiddleware([/* middlewares jetstream, si applicable */]);
+    Route::get('/movies/{id}', [MovieController::class, 'show'])->name('movies.show');
